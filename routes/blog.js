d82213170ba8e3e5a974aeb30ac0c0a0c0ad86d5
Feroz <mdferozahmed27156@gmail.com>
2025-05-23 18:38:02 +0530

    Update blog.js

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
 const multer = require("multer");
-const path = require("path");
 const {
   addNewGet,
   addNewPost,
@@ -8,16 +7,8 @@ const {
   getComments,
 } = require("../controllers/blog");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.resolve(`./public/uploads`));
-  },
-  filename: function (req, file, cb) {
-    const filename = `${Date.now()}-${file.originalname}`;
-    cb(null, filename);
-  },
-});
-const upload = multer({ storage: storage });
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 const router = Router();
 
@@ -26,7 +17,7 @@ router.get("/addnew", addNewGet);
 router.post("/addnew", upload.single("coverImageUrl"), addNewPost);
 
 router.get("/:id", getBlog);
-
 router.post("/comment/:blogId", getComments);
 
 module.exports = router;
+
