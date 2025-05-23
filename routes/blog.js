79ffca88ac85bf01c7139f253e9f8ba6868033a5
Feroz <mdferozahmed27156@gmail.com>
2025-05-23 16:07:04 +0530

    Update blog.js

--- routes/blog.js
+++ routes/blog.js
@@ -10,7 +10,7 @@ const {
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve(`./public/uploads`));
+    cb(null, path.resolve(`/app/public/uploads`));
   },
   filename: function (req, file, cb) {
     const filename = `${Date.now()}-${file.originalname}`;
